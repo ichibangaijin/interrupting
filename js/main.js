a7f3f6ecfe54e7d6018ab54e8d1ae4097f957f98
Matt Highland <ichibangaijin77@gmail.com>
2016-10-31 13:08:42 -0700

    need more money

--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@ setTimeout(interruptMe, 5000);
 
 var counter = 0;
 function interruptMe(){
+	while(counter < 3){
 	var interrupt = prompt("We are sorry, our password system is malfunctioning.  Please enter password.");
 	alert("That is was the old password.  The new password is " + interrupt);
-	
-	while(counter < 3){
-		interruptMe();
+		
 		counter++;
 	}
 	alert("Computer is now refusing to work with you");
-	document.write("Please come back with more money to spend.");
+	document.write("Please come back with more money. And put it in my disk drive.");
 	
 }
