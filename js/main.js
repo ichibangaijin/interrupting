c87b634e15dee4e98a6cfddd9b7b568b60ddfd8a
Matt Highland <ichibangaijin77@gmail.com>
2016-10-31 13:04:25 -0700

    awful update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+setTimeout(interruptMe, 5000);
 
+var counter = 0;
+function interruptMe(){
+	var interrupt = prompt("We are sorry, our password system is malfunctioning.  Please enter password.");
+	alert("That is was the old password.  The new password is " + interrupt);
+	
+	while(counter < 3){
+		interruptMe();
+		counter++;
+	}
+	alert("Computer is now refusing to work with you");
+	document.write("Please come back with more money to spend.");
+	
+}
